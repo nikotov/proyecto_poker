ccd890fcf119286ea010eb146bfb9a1f854491d6
JuanBurgosLandazuri <juandaburgoslandazuri@gmail.com>
2024-04-25 11:11:52 -0500

    Me la chupa la poroga

---
+++ functions.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+// ASCII chars for suits
+    char hearts[] = "\x03";
+    char diamonds[] = "\x04";
+    char clubs[] = "\x05";
+    char spades[] = "\x06";
+
+
+int getValue(int card) {
+    //returns numerical value of a card within a certain suit
+    return card % 13 + 1;
+}
+
+
+string getSuit(int card) {
+    //0-12: Hearts, 13-25: Diamonds, 26-38: Clubs, 39-51: Spades
+    int x = card / 13;
+    if (x == 0) return hearts;
+    if (x == 1) return diamonds;
+    if (x == 2) return clubs;
+    return spades;
+
+}
+
+
+void shuffleVec(vector<int>& vec) {
+    // Seed for srand
+    srand((unsigned) time(0));
+
+    // Fisher-Yates shuffle algorithm
+    for (int i = vec.size() - 1; i > 0; --i) {
+        int j = rand() % (i + 1);
+        swap(vec[i], vec[j]);
+    }
+}
+
+
+void printHand(vector<int>hand){
+
+    for(int i = 0; i < hand.size(); i++){
+        cout << i+1 << ".) ";
+
+        //Handles non-numerical cards
+        switch(getValue(hand[i])){
+            case 1:
+                cout << setw(5) << "Ace";
+                break;
+
+            case 11: 
+                cout << setw(5) << "Jack";
+                break;
+                                
+            case 12:
+                cout << setw(5) << "Queen";
+                break;
+                                
+            case 13:
+                cout << setw(5) << "King";
+                break;
+            
+            default:
+                cout << setw(5) << getValue(hand[i]);                           
+        }
+        cout << " of " << getSuit(hand[i]) << endl;
+    }
+}
+
+
+void dealHand(vector<int>& deck, vector<int>& hand){
+    const int handSize = 5;
+
+    //Pass the first value of shuffled deck to hand, removes card from deck
+    for(int i = 0; i < handSize; i++){
+        hand.push_back(deck[0]);
+        deck.erase(deck.begin());
+    }
+}
+
+
+void sortHand(vector<int>& hand){
+
+    //Selection Sort algorithm using card value 
+    for (int i = 0; i < hand.size() - 1; ++i) {
+            int minInd = i;
+            for (int j = i + 1; j < hand.size(); ++j) {
+                if (getValue(hand[j]) < getValue(hand[minInd])) minInd = j;
+            }
+            if (minInd != i) {
+                swap(hand[i], hand[minInd]);
+            }
+        }
+
+}
+
+
+bool nthOfAKind(int n, vector<int>hand){
+    int counter;
+    int compare;
+    for(int i = 0; i < hand.size()+1 - n; i++){
+        counter = 1;
+        int compare = getValue(hand[i]);
+        for(int j = i+1; j < hand.size(); j++){
+            if(getValue(hand[j]) == compare) counter++;
+        }
+        if(counter >= n) break;
+    }
+
+    if(counter >= n) return getValue(compare);
+    else return false;
+}
+
+
+bool twoPair(vector<int>hand){
+    int pairs = 0;
+    for(int i = 0; i < hand.size()-1; i++){
+        int compare = getValue(hand[i]);
+        for(int j = i+1; j < hand.size(); j++){
+            if(getValue(hand[j]) == compare) pairs++;
+        }
+        if(pairs >= 2) break;
+    }
+
+    if(pairs >= 2) return true;
+    else return false;
+}
+
--- main.cpp
+++ main.cpp
@@ -1,79 +1,50 @@
-/* Nicolás Tovar    NRC 3455    00/00/2024
-Purpose*/
-
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-void shuffleVector(vector<int>& vec);
+void shuffleVec(vector<int>& vec);
+
 string getSuit(int card);
+
 int getValue(int card);
 
-// ASCII chars for suits
-    char hearts[] = "\x03";
-    char diamonds[] = "\x04";
-    char clubs[] = "\x05";
-    char spades[] = "\x06";
+void printHand(vector<int>hand);
 
+void dealHand(vector<int>& deck, vector<int>& hand);
+
+void sortHand(std::vector<int>& hand); 
+
+bool nthOfAKind(int n, vector<int>hand);
 
 int main() {
-    bool running = true;
 
     // Ordered deck
     vector<int> deck = {
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
         21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
-        39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52
+        39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
     };
 
-    vector <int> shuffledDeck;
-    shuffledDeck = deck;
-
-    shuffleVector(shuffledDeck);
-
-    while (running) {
-        for(int k = 0, card; k < deck.size(); k++) {
-            card = deck[k];
-            cout << card << ": " << getSuit(card) << getValue(card) << endl;  
-        }
-        cout << endl << endl;
-
-        for(int k = 0, card; k < shuffledDeck.size(); k++) {
-            card = shuffledDeck[k];
-            cout << card << ": " << getSuit(card) << getValue(card) << endl;  
-        }
-
-        running = false;   
-    }
+    vector<int> player;
+    vector<int> computer;
     
+    shuffleVec(deck);
+
+    dealHand(deck, player);
+    cout << "Player Deck: " << endl;
+    printHand(player);
+
+    sortHand(player);
+
+    cout << "Sorted Player Deck: " << endl;
+    printHand(player);
+
+    cout << boolalpha;
+    cout << nthOfAKind(3, player);
+
     return 0;
 }
 
-int getValue(int card) {
-    return card % 13 + 2;
-}
-
-string getSuit(int card) {
-    int x = card / 13;
-    if (x == 0) return hearts;
-    if (x == 1) return diamonds;
-    if (x == 2) return clubs;
-    return spades;
-
-}
-
-
-void shuffleVector(vector<int>& vec) {
-    // Seed for srand
-    srand((unsigned) time(0));
-
-    // Fisher-Yates shuffle algorithm
-    for (int i = vec.size() - 1; i > 0; --i) {
-        int j = rand() % (i + 1);
-        swap(vec[i], vec[j]);
-    }
-}
